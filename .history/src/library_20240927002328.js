31b4e2ddfe6482c06eff02111badfbad08fc64fd
RushikeshE <rushikeshedne01@gmail.com>
2024-09-27 00:25:54 +0530

    Implement add book feature in our system

---
+++ .history/src/library_20240927002328.js
@@ -0,0 +1,16 @@
+class Library {
+    constructor() {
+      this.books = [];
+    }
+  
+    addBook(book) {
+      this.books.push(book);
+    }
+  
+    getBooks() {
+      return this.books;
+    }
+  }
+  
+  module.exports = Library;
+  
